0a024c501894a329a5854b76672cdc48a5ea1201
Karolin <karolin.thunman@gmail.com>
2023-03-23 13:37:02 +0100

    betting form etc

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,44 +1,65 @@
 import { Link } from "react-router-dom";
 import karrosProfilePicture from "./assets/karro.png";
+import useMediaQuery from "./hooks/useMediaQuery";
 
-const Header = () => (
-  <header className="fixed px-8 py-2  w-full  bg-white drop-shadow-md z-10">
-    {/* <header className="sticky px-8 py-3 max-w-7xl mx-auto w-full "> */}
-    <nav className="flex justify-between items-center text-slate-800 max-w-7xl mx-auto">
-      <div className="flex items-center">
-        <Link to={`/`} className="text-4xl text-pink-600 mr-6">
-          SBL
-        </Link>
-        <Link to={"/stats"} className="mr-6">
-          Stats
-        </Link>
-        <Link to="/history">Previous weeks</Link>
-      </div>
-      <Link to={`/user`}>
-        {karrosProfilePicture ? (
-          <img
-            src={karrosProfilePicture}
-            alt=""
-            className="h-12 rounded-full border-2 border-gray-400 bg-gray-400"
-          />
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            className="w-14 h-14 stroke-slate-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-        )}
-      </Link>
-    </nav>
-  </header>
+export const sizes = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+const Menu = () => (
+  <>
+    {" "}
+    <Link to={"/stats"} className="mr-6">
+      Stats
+    </Link>
+    <Link to="/history">Previous weeks</Link>
+  </>
 );
+
+const Header = () => {
+  const bigScreen = useMediaQuery(`(min-width: ${sizes.md})`);
+
+  return (
+    <header className="fixed px-8 py-2  w-full  bg-white drop-shadow-md z-10">
+      {/* <header className="sticky px-8 py-3 max-w-7xl mx-auto w-full "> */}
+      <nav className="flex justify-between items-center text-slate-800 max-w-7xl mx-auto">
+        <div className="flex items-center">
+          <Link to={`/`} className="text-4xl text-pink-600 mr-6">
+            SBL
+          </Link>
+          {bigScreen ? <Menu /> : null}
+        </div>
+
+        <Link to={`/user`}>
+          {karrosProfilePicture ? (
+            <img
+              src={karrosProfilePicture}
+              alt=""
+              className="h-12 rounded-full border-2 border-gray-400 bg-gray-400"
+            />
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="w-14 h-14 stroke-slate-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          )}
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- client/src/UserPage.tsx
+++ client/src/UserPage.tsx
@@ -1,39 +1,91 @@
+import { useReducer } from "react";
+
+const SET_BET: string = "SET_BET";
+const SET_GAMES_TO_BET: string = "SET_GAMES_TO_BET";
+
+function gamesToBetReducer(state, action) {
+  switch (action.type) {
+    case SET_BET: {
+      return state;
+    }
+    case SET_GAMES_TO_BET: {
+      return state;
+    }
+    default:
+      return state;
+  }
+}
+
 const UserPage = () => {
+  const [gamesToBet, dispatch] = useReducer(gamesToBetReducer, []);
   return (
     <>
-      <div className="flex justify-center my-4">
-        <button className="h-10 w-24 flex justify-center items-center text-center  rounded-sm bg-white">
-          Bears
+      <div className="flex justify-center my-4 items-center">
+        <button className="h-12 w-24 flex justify-center items-center text-center  rounded-sm bg-white">
+          <img
+            src="https://static.www.nfl.com/image/private/f_auto/league/teguylrnqqmfcwxvcmmz"
+            alt=""
+            className="h-8 mr-2 opacity-40"
+          />
+          <div className="text-gray-400">
+            <div>SF</div>
+            <div className="text-sm">2.25</div>
+          </div>
         </button>
-        <div className="h-10 w-10 flex justify-center items-center text-center ">
+        <div className="h-12 w-10 flex justify-center items-center text-center ">
           @
         </div>
-        <button className="h-10 w-24 flex justify-center items-center text-center  rounded-sm bg-orange-200">
-          Packers
+        <button className="h-12 w-24 flex justify-center items-center text-center  rounded-sm bg-white ring-offset-2 ring-2 ring-orange-500">
+          <img
+            src="https://static.www.nfl.com/image/private/f_auto/league/teguylrnqqmfcwxvcmmz"
+            alt=""
+            className="h-8 mr-2"
+          />
+          <div>
+            <div>GB</div>
+            <div className="text-sm">1.00</div>
+          </div>
         </button>
         <input
           type="number"
           className="h-10 w-20 mx-4 text-right p-2 shadow-inner rounded-sm"
+          value={100}
         ></input>
       </div>
-      <div className="flex justify-center my-4">
-        <button className="h-10 w-24 flex justify-center items-center text-center  rounded-sm bg-white">
-          Bears
+      <div className="flex justify-center my-4 items-center">
+        <button className="h-12 w-24 flex justify-center items-center text-center  rounded-sm bg-white">
+          <img
+            src="https://static.www.nfl.com/image/private/f_auto/league/teguylrnqqmfcwxvcmmz"
+            alt=""
+            className="h-8 mr-2"
+          />
+          CHI
         </button>
-        <div className="h-10 w-10 flex justify-center items-center text-center ">
+        <div className="h-12 w-10 flex justify-center items-center text-center ">
           @
         </div>
-        <button className="h-10 w-24 flex justify-center items-center text-center  rounded-sm bg-white">
-          Packers
+        <button className="h-12 w-24 flex justify-center items-center text-center  rounded-sm bg-white">
+          <img
+            src="https://static.www.nfl.com/image/private/f_auto/league/teguylrnqqmfcwxvcmmz"
+            alt=""
+            className="h-8 mr-2"
+          />
+          PHI
         </button>
-        <input
-          type="number"
-          className="h-10 w-20 mx-4 text-right p-2 shadow-inner rounded-sm"
-        ></input>
+        <div className="relative">
+          <input
+            type="number"
+            className="h-10 w-20 mx-4 text-right p-2 shadow-inner rounded-sm"
+            value={250}
+          ></input>
+          <span className="absolute top-0 left-5 text-xs text-gray-600">
+            890
+          </span>
+        </div>
       </div>
 
       <button className="bg-slate-300 rounded-sm py-2 px-4 mr-2">Save</button>
-      <button className="bg-pink-500 text-white rounded-sm py-2 px-4">
+      <button className="bg-orange-500 text-white rounded-sm py-2 px-4">
         Submit
       </button>
     </>
